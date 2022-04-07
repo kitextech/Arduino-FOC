4a02061a456f096dbd9be407dc153afeeae69810
aokholm <andreas@kitex.tech>
2022-04-07 14:29:27 +0200

    removed delay from initialization

--- src/StepperMotor.cpp
+++ src/StepperMotor.cpp
@@ -52,11 +52,11 @@ void StepperMotor::init() {
   }
   P_angle.limit = velocity_limit;
 
-  _delay(500);
+  // _delay(500);
   // enable motor
   if(monitor_port) monitor_port->println(F("MOT: Enable driver."));
   enable();
-  _delay(500);
+  // _delay(500);
 
   motor_status = FOCMotorStatus::motor_uncalibrated;
 }
